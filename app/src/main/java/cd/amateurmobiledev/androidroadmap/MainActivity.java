43019ab19b1c9ca3558b8a6f1a399f033f0aed95
josh muleshi <jmuleshi2@gmail.com>
2021-01-28 10:25:46 +0000

    programming the first file

--- app/src/main/java/cd/amateurmobiledev/androidroadmap/MainActivity.java
+++ app/src/main/java/cd/amateurmobiledev/androidroadmap/MainActivity.java
@@ -2,13 +2,28 @@ package cd.amateurmobiledev.androidroadmap;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import java.util.Objects;
+import java.util.Timer;
+
 public class MainActivity extends AppCompatActivity {
+
+    Timer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        timer = new Timer();
+        timer.schedule(() -> {
+            Intent i = new Intent(this,);
+            startActivity(i);
+            finish();
+        }, 5000);
+
     }
 }
