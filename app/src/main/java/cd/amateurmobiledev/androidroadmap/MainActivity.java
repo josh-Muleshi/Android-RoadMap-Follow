78b251bb3c2066c521ffd517bb553ece839ac63e
josh muleshi <jmuleshi2@gmail.com>
2021-02-05 12:43:24 +0000

    Reference the graphic elements in your activity

--- app/src/main/java/cd/amateurmobiledev/androidroadmap/MainActivity.java
+++ app/src/main/java/cd/amateurmobiledev/androidroadmap/MainActivity.java
@@ -2,13 +2,24 @@ package cd.amateurmobiledev.androidroadmap;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView GreetingTxt;
+    private EditText NameInput;
+    private Button PlayBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GreetingTxt = findViewById(R.id.greeting_txt);
+        NameInput = findViewById(R.id.name_input);
+        PlayBtn = findViewById(R.id.play_btn);
 
     }
 }
