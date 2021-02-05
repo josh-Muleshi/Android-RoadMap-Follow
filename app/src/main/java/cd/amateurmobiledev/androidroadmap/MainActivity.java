f9859f8a2fdbf796fbec994d30b370472ddac7e0
josh muleshi <jmuleshi2@gmail.com>
2021-02-05 12:59:25 +0000

    Manage user actions

--- app/src/main/java/cd/amateurmobiledev/androidroadmap/MainActivity.java
+++ app/src/main/java/cd/amateurmobiledev/androidroadmap/MainActivity.java
@@ -2,6 +2,9 @@ package cd.amateurmobiledev.androidroadmap;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -20,6 +23,32 @@ public class MainActivity extends AppCompatActivity {
         GreetingTxt = findViewById(R.id.greeting_txt);
         NameInput = findViewById(R.id.name_input);
         PlayBtn = findViewById(R.id.play_btn);
+
+        PlayBtn.setEnabled(false);
+
+        NameInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                PlayBtn.setEnabled(charSequence.toString().length() != 0);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        PlayBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO
+            }
+        });
 
     }
 }
