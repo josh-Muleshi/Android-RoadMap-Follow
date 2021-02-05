7ff729b331fcf088fde94def9de53ccf9b7a1109
josh muleshi <jmuleshi2@gmail.com>
2021-02-05 11:45:35 +0000

    first commit for the new goals i fixed

--- app/src/main/java/cd/amateurmobiledev/androidroadmap/MainActivity.java
+++ app/src/main/java/cd/amateurmobiledev/androidroadmap/MainActivity.java
@@ -11,45 +11,10 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView txtNom;
-    private TextView txtDesc;
-    private TextView txtLoc;
-    private TextView txtWebSite;
-    private TextView txtDate;
-
-    private Button btnEdit;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txtDesc = findViewById(R.id.txtDescription);
-        txtLoc = findViewById(R.id.txtLocalisation);
-        txtWebSite = findViewById(R.id.txtWebSite);
-        txtDate = findViewById(R.id.txtDate);
-
-        btnEdit = findViewById(R.id.btnEdit);
-        btnEdit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
-                startActivityForResult(intent, 15);
-            }
-        });
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 15){
-            if (resultCode == 12) {
-                txtNom = findViewById(R.id.txtNom);
-                String nom = data.getStringExtra("nom");
-                String desc = data.getStringExtra("desc");
-                txtNom.setText(nom);
-                txtDesc.setText(desc);
-            }
-        }
     }
 }
