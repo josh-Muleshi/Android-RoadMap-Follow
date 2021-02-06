571ec3d1c101d0734ac3d668d40248efbbaa38db
josh muleshi <jmuleshi2@gmail.com>
2021-02-06 10:39:45 +0000

    add questions

--- app/src/main/java/cd/amateurmobiledev/androidroadmap/controller/GameActivity.java
+++ app/src/main/java/cd/amateurmobiledev/androidroadmap/controller/GameActivity.java
@@ -6,7 +6,12 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Arrays;
+import java.util.List;
+
 import cd.amateurmobiledev.androidroadmap.R;
+import cd.amateurmobiledev.androidroadmap.model.Question;
+import cd.amateurmobiledev.androidroadmap.model.QuestionBanck;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -16,15 +21,65 @@ public class GameActivity extends AppCompatActivity {
     private Button btnAns3;
     private Button btnAns4;
 
+    private QuestionBanck questionBanck;
+    private Question currentQuestion;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        questionBanck = this.generateQuestions();
 
         txtQuestion = findViewById(R.id.question_text);
         btnAns1 = findViewById(R.id.answer1_btn);
         btnAns2 = findViewById(R.id.answer2_btn);
         btnAns3 = findViewById(R.id.answer3_btn);
         btnAns4 = findViewById(R.id.answer4_btn);
+
+        btnAns1.setTag(0);
+        btnAns2.setTag(1);
+        btnAns3.setTag(2);
+        btnAns4.setTag(3);
+
+        currentQuestion = questionBanck.getQuestion();
+        this.displayQuestion(currentQuestion);
+
+    }
+
+    public void displayQuestion(final Question question){
+        txtQuestion.setText(question.getQuestion());
+        btnAns1.setText(question.getChoiceList().get(0));
+        btnAns2.setText(question.getChoiceList().get(1));
+        btnAns3.setText(question.getChoiceList().get(2));
+        btnAns4.setText(question.getChoiceList().get(3));
+    }
+
+    private QuestionBanck generateQuestions(){
+
+        Question question1 = new Question("Who is the creator of Android?",
+                Arrays.asList("Andy Rubin",
+                        "Steve Wozniak",
+                        "Jake Wharton",
+                        "Paul Smith"),
+                0);
+
+        Question question2 = new Question("When did the first man land on the moon?",
+                Arrays.asList("1958",
+                        "1962",
+                        "1967",
+                        "1969"),
+                3);
+
+        Question question3 = new Question("What is the house number of The Simpsons?",
+                Arrays.asList("42",
+                        "101",
+                        "666",
+                        "742"),
+                3);
+
+        return new QuestionBanck(Arrays.asList(question1,
+                question2,
+                question3));
     }
 }
