bb8704e6d8b331923195514b476d9da14b507e82
josh muleshi <jmuleshi2@gmail.com>
2021-02-06 09:54:11 +0000

    implementation of a Model class named User

---
+++ app/src/main/java/cd/amateurmobiledev/androidroadmap/model/User.java
@@ -0,0 +1,14 @@
+package cd.amateurmobiledev.androidroadmap.model;
+
+public class User {
+
+    private String firstName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
